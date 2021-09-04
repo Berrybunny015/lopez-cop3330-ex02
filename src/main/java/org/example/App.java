24c5ed8d1e639860f843329ba9058b83fad4b526
Berrybunny015 <berrybunny015@gmail.com>
2021-09-04 19:01:07 -0400

    couple edits

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,17 +4,15 @@ import java.util.Scanner;
 
 public class App {
     public static void main( String[] args ) {
-        //variable declaration
-        int numChars = 0;
 
         System.out.println("\nWhat is the input string? "); //output #1
 
         Scanner input = new Scanner(System.in); //input
         String stringName = input.next();
 
-        numChars = stringName.length();
+        int numChars = stringName.length();
 
-        System.out.println(stringName + " has " + numChars + " characters."); //output #2
+        System.out.println("\n" + stringName + " has " + numChars + " characters."); //output #2
 
     }
 }
